3f5d8b13f3adbf1a9556e52403e97c788a7bc3e6
sridharan <sridharan.m@hotmail.com>
2024-02-22 23:52:31 +0530

    Updates

---
+++ precache-manifest.1fb5445c31c9a881d0c759bd880c56a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d303e2e9f3242412b4852dc2f6a6db7",
+    "url": "/SridharanMu/React_StudentRegistration/index.html"
+  },
+  {
+    "revision": "c444914a056e95097d5e",
+    "url": "/SridharanMu/React_StudentRegistration/static/css/2.11d253d1.chunk.css"
+  },
+  {
+    "revision": "77f1f74f2b8196de16ee",
+    "url": "/SridharanMu/React_StudentRegistration/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "c444914a056e95097d5e",
+    "url": "/SridharanMu/React_StudentRegistration/static/js/2.6c51cd49.chunk.js"
+  },
+  {
+    "revision": "e112c437d1f30a2986f955140ac7f302",
+    "url": "/SridharanMu/React_StudentRegistration/static/js/2.6c51cd49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33a97be8cd2f9318118",
+    "url": "/SridharanMu/React_StudentRegistration/static/js/3.41f7e31a.chunk.js"
+  },
+  {
+    "revision": "77f1f74f2b8196de16ee",
+    "url": "/SridharanMu/React_StudentRegistration/static/js/main.3da2e177.chunk.js"
+  },
+  {
+    "revision": "fec453e964f4ddb73bb5",
+    "url": "/SridharanMu/React_StudentRegistration/static/js/runtime-main.e66666d4.js"
+  }
+]);
